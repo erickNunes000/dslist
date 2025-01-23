45db82c07cb7d8754489740bf79d50c5287cd037
Erick Nunes <erickFernandes000@gmail.com>
2025-01-23 09:28:23 -0300

    GameMinProjection, search games by list

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -22,8 +22,9 @@ public class GameListService {
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll(){	 
 		List<GameList> result = gameListRepository.findAll();
-		
 		List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
 		return dto;
 	}
+	
+	
 }
